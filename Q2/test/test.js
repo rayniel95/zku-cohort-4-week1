e748c62f1e065bda046d367e53e2d7df914058d9
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-05-08 20:59:18 -0400

    comment what is doing in each line

--- Q2/test/test.js
+++ Q2/test/test.js
@@ -1,9 +1,10 @@
 const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
+// load snarkjs
 const { groth16 } = require("snarkjs");
-
+// load a tool that allow to execute some methods in a circom circuit
 const wasm_tester = require("circom_tester").wasm;
-
+// NOTE - i have'nt idea
 const F1Field = require("ffjavascript").F1Field;
 const Scalar = require("ffjavascript").Scalar;
 exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
@@ -15,22 +16,25 @@ describe("HelloWorld", function () {
     let verifier;
 
     beforeEach(async function () {
+        //NOTE - deploy the verifier in a local blockchain
         Verifier = await ethers.getContractFactory("HelloWorldVerifier");
         verifier = await Verifier.deploy();
         await verifier.deployed();
     });
 
     it("Circuit should multiply two numbers correctly", async function () {
+        //NOTE - load the circom circuit
         const circuit = await wasm_tester("contracts/circuits/HelloWorld.circom");
 
         const INPUT = {
             "a": 2,
             "b": 3
         }
-
+        //NOTE - calculate the witness. it have 1 in the first position, 
+        // the output signal value in the second and the input signal value in the next
         const witness = await circuit.calculateWitness(INPUT, true);
 
-        //console.log(witness);
+        // console.log(witness);
 
         assert(Fr.eq(Fr.e(witness[0]),Fr.e(1)));
         assert(Fr.eq(Fr.e(witness[1]),Fr.e(6)));
@@ -39,12 +43,14 @@ describe("HelloWorld", function () {
 
     it("Should return true for correct proof", async function () {
         //[assignment] Add comments to explain what each line is doing
+        //NOTE - calcualte the witness and generate the proof
         const { proof, publicSignals } = await groth16.fullProve({"a":"2","b":"3"}, "contracts/circuits/HelloWorld/HelloWorld_js/HelloWorld.wasm","contracts/circuits/HelloWorld/circuit_final.zkey");
 
         console.log('2x3 =',publicSignals[0]);
-        
+        //NOTE - using the proof and the public signals a calldata to send to de
+        // verifier smart contract is created
         const calldata = await groth16.exportSolidityCallData(proof, publicSignals);
-    
+        //NOTE - this seem to fixed some type errors
         const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
     
         const a = [argv[0], argv[1]];
