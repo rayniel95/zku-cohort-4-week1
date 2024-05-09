9a6a1d428fc5b551cc3d7346d22c5a6eda5e172d
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-05-08 20:58:44 -0400

    fix reges

--- Q2/scripts/bump-solidity.js
+++ Q2/scripts/bump-solidity.js
@@ -1,12 +1,28 @@
 const fs = require("fs");
-const solidityRegex = /pragma solidity \^\d+\.\d+\.\d+/
+const solidityRegex = /^pragma solidity [^;]+( [^;]+)*;$/
 
-const verifierRegex = /contract Verifier/
+const verifierRegex = /contract\s+([a-zA-Z_$][a-zA-Z_$0-9]*)/
 
-let content = fs.readFileSync("./contracts/HelloWorldVerifier.sol", { encoding: 'utf-8' });
-let bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0');
+let content = fs.readFileSync("./contracts/HelloWorldVerifier.sol", 'utf-8');
+
+let bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0;');
 bumped = bumped.replace(verifierRegex, 'contract HelloWorldVerifier');
 
 fs.writeFileSync("./contracts/HelloWorldVerifier.sol", bumped);
 
 // [assignment] add your own scripts below to modify the other verifier contracts you will build during the assignment
+
+content = fs.readFileSync("./contracts/Multiplier3Verifier_plonk.sol", 'utf-8');
+
+bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0;');
+bumped = bumped.replace(verifierRegex, 'contract Multiplier3Verifier_plonk');
+
+fs.writeFileSync("./contracts/Multiplier3Verifier_plonk.sol", bumped);
+// ---------------
+
+content = fs.readFileSync("./contracts/Multiplier3Verifier.sol", 'utf-8');
+
+bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0;');
+bumped = bumped.replace(verifierRegex, 'contract Multiplier3Verifier');
+
+fs.writeFileSync("./contracts/Multiplier3Verifier.sol", bumped);
