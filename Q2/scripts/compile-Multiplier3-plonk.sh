2f9f0a4180b3abf8e298952b22efc617034d9e68
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-05-13 19:05:24 -0400

    fix circom plonk setup and generation key

--- Q2/scripts/compile-Multiplier3-plonk.sh
+++ Q2/scripts/compile-Multiplier3-plonk.sh
@@ -16,12 +16,12 @@ echo "Compiling Multiplier3_plonk.circom..."
 
 # compile circuit
 
-circom Multiplier3.circom --r1cs --wasm --sym -o Multiplier3_plonk
-snarkjs r1cs info Multiplier3_plonk/Multiplier3.r1cs
+circom Multiplier3_plonk.circom --r1cs --wasm --sym -o Multiplier3_plonk
+snarkjs r1cs info Multiplier3_plonk/Multiplier3_plonk.r1cs
 
 # Start a new zkey and make a contribution
 
-snarkjs plonk setup Multiplier3_plonk/Multiplier3.r1cs powersOfTau28_hez_final_10.ptau Multiplier3_plonk/circuit_0000.zkey
+snarkjs plonk setup Multiplier3_plonk/Multiplier3_plonk.r1cs powersOfTau28_hez_final_10.ptau Multiplier3_plonk/circuit_0000.zkey
 # snarkjs zkey contribute Multiplier3_plonk/circuit_0000.zkey Multiplier3_plonk/circuit_final.zkey --name="1st Contributor Name" -v -e="rainyel ramos"
 snarkjs zkey export verificationkey Multiplier3_plonk/circuit_0000.zkey Multiplier3_plonk/verification_key.json
 
